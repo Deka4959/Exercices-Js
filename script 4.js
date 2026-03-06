1e2244b50a6a264df5a51d965ce4a8ba78baf209
Deka <suheeypcade143@gmail.com>
2026-03-06 15:48:28 +0100

    add new JS exercise

---
+++ script 4.js
@@ -0,0 +1,30 @@
+function add (a,b){
+    
+    return a+b;
+   
+   
+
+}
+
+ console.log(add(5,10));
+ console.log(add(30,9));
+ console.log(add(7,8));
+ 
+
+
+
+ 
+ 
+
+
+
+//  function expression
+
+const city = function(CityName){
+    console.log("city " +CityName);
+
+}
+city("Amsterdam");
+city("Newyork");
+city("Atlanta");
+city("Losangels");

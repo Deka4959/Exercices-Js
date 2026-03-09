fe1d900853200257d48b8815db77d1338c1c60cc
Deka <suheeypcade143@gmail.com>
2026-03-09 17:24:33 +0100

    add new exercise

--- Js Ex 5.js
+++
@@ -1,8 +0,0 @@
-
-const add = (name)=>{
-    console.log("hello " + name);
-}
-add("Deka")
-add("ustad Hamuuda")
-add("ustad shrafdiin")
-add("ustad omar tood");
---
+++ jsEx 6 & 7.js
@@ -0,0 +1,24 @@
+//Excecise 6
+
+let colors = ["green","blue" ,"red" ,"yellow"];
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+
+//EXcecise 7 methods
+
+colors.push("pink");
+
+console.log(colors);
+
+
+colors.pop();
+console.log(colors);
+colors.shift();
+console.log(colors);
+
+colors.unshift("gray");
+console.log(colors);
+console.log(colors.length)
+console.log(colors)

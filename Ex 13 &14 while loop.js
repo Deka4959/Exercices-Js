cd1c2dd97458301a1d1aa0a9aac8598fb7f2b89a
Deka <suheeypcade143@gmail.com>
2026-03-14 21:56:50 +0100

    JS exercise 13 and 14

---
+++ Ex 13 &14 while loop.js
@@ -0,0 +1,25 @@
+
+
+// exercise 13
+
+ let i =5;
+ while(i > 1){
+
+    console.log(i)
+
+     i--;
+
+
+}
+
+
+
+// exercise 14
+
+let number;
+
+do {
+    number = prompt("Enter number greater than 10");
+
+}
+while(number <= 10);
--- for loop.js
+++
@@ -1,7 +0,0 @@
-const fruits =["Apple 🍎", "Banana 🍌", "Strawberry 🍓"]
-
-
-for(let i = 0; i < fruits.length; i++){
-    console.log(fruits[i]);
-
-}
